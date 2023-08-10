2c9e67d20cb3e105ab917f607ee74a07a28aa1e3
jman13378 <garconiamail@gmail.com>
2023-08-10 09:34:53 -0400

    update libraries/PROS

---
+++ git.bat
@@ -0,0 +1,6 @@
+ECHO off
+git add -A
+set /P "id=Enter message (Enter N6ED to commit with the default message): "
+
+
+exit
--- src/m.h
+++ src/m.h
@@ -1,13 +1,14 @@
 
 
 // include okapi and pros
-
+// the library itself
 #include "main.h"
 
 // include utils
 
 
 // controller
+// better to init in this file than the ports file
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER); 
 
