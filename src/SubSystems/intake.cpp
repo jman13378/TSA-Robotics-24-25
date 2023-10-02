1dae73943da77a8c2cbe6ccfb9da7ddf9bc29006
jman13378 <garconiamail@gmail.com>
2023-10-02 13:14:08 -0400

    e

--- src/SubSystems/intake.cpp
+++ src/SubSystems/intake.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#define range [111,77]
+
 //     Intake.move((127*controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2))-(127*controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)));
 
 void setIntake(int power)
@@ -9,7 +11,7 @@ void setIntake(int power)
 bool isTriBall(pros::Optical op)
 {
     double hue = op.get_hue();
-    return (hue <= 111 && hue >= 77);
+    return (hue <= range[0] && hue >= range[1]);
 }
 void setIntakeMotor()
 {
